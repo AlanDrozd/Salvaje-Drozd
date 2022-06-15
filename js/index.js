dc5ce3620ddc8acc59f7ef9f3f6a8761e4c9912e
Alan Drozd <alanezedrozd@gmail.com>
2022-06-14 23:40:50 -0300

    editado index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-alert ("Coderhourse 2022 - Desafio N° 1+2 - Alan Drozd");
+alert ("Coderhourse 2022 - Desafio N° 1+2+3 - Alan Drozd");
 alert ("Para visualizar este sitio por favor ingresa: \nUsuario:coder \nClave:house");
 
 let usuario = prompt("Ingrese Usuario: ");
@@ -21,28 +21,39 @@ if (edad >17) {
     alert("Por favor en caso de concretar una compra, consultalo antes con un adulto.")
 };
 
-function productos(nombre, importe) {
-    this.nombre = nombre;
-    this.importe = importe;
+const Ropa = []
+
+class Producto {
+    constructor(nombre, importe){
+        this.nombre = nombre;
+        this.importe = importe;  
+    }
+    mostrarProducto(){
+        return this.nombre;
+    }
 }
-const polerinYaro = new productos('Polerin Yaro Negro', 3790);
-const poleronDeanCrudo = new productos('Poleron Dean Crudo', 8990);
-const jeanJacob = new productos('Jean Jacob', 8990);
-const poleronDeanNegro = new productos('Poleron Dean Negro', 8990);
+
+Ropa.push (new Producto('Polerin Yaro Negro', 3790));
+Ropa.push (new Producto('Poleron Dean Crudo', 8990));
+Ropa.push (new Producto('Jean Jacob', 8990));
+Ropa.push (new Producto('Poleron Dean Negro', 8990));
+
+console.log (Ropa[2].nombre)
+
 
 let nombre = prompt("Hola salvaje! Antes de continuar, podrias decirnos tu nombre?");
-let saludo =  alert (nombre + ", bienvenido a nuestra tienda!");
-let pedido = prompt("Cual de nuestros productos te interesa?" + "\n" + "1- " + polerinYaro.nombre + " $" + polerinYaro.importe + "\n" + "2- " + poleronDeanCrudo.nombre + " $" + poleronDeanCrudo.importe + "\n" + "3- " + jeanJacob.nombre + " $" + jeanJacob.importe + "\n" + "4- " + poleronDeanNegro.nombre + " $" + poleronDeanNegro.importe + "\n");
+let saludo = alert (nombre + ", bienvenido a nuestra tienda!");
+let pedido = prompt("Cual de nuestros productos te interesa?" + "\n" + "1- " + Ropa[0].nombre + " $" + Ropa[0].importe + "\n" + "2- " + Ropa[1].nombre + " $" + Ropa[1].importe + "\n" + "3- " + Ropa[2].nombre + " $" + Ropa[2].importe + "\n" + "4- " + Ropa[3].nombre + " $" + Ropa[3].importe + "\n");
 let cantidad = prompt("Cuantos queres comprar?");
 
 if (pedido == 1) {
-    alert("Seleccionaste: " + polerinYaro.nombre + ", el total de tu pedido es de $" + polerinYaro.importe * cantidad);
+    alert("Seleccionaste: " + Ropa[0].nombre + ", el total de tu pedido es de $" + Ropa[0].importe * cantidad);
     } else if (pedido == 2) {
-        alert("Seleccionaste: " + poleronDeanCrudo.nombre + ", el total de tu pedido es de $" + poleronDeanCrudo.importe * cantidad);
+        alert("Seleccionaste: " + Ropa[1].nombre + ", el total de tu pedido es de $" + Ropa[1].importe * cantidad);
     } else if (pedido == 3) {
-        alert("Seleccionaste: " + jeanJacob.nombre + ", el total de tu pedido es de $" + jeanJacob.importe * cantidad);
+        alert("Seleccionaste: " + Ropa[2].nombre + ", el total de tu pedido es de $" + Ropa[2].importe * cantidad);
     } else if (pedido == 4) {    
-        alert("Seleccionaste: " + poleronDeanNegro.nombre + ", el total de tu pedido es de $" + poleronDeanNegro.importe * cantidad);
+        alert("Seleccionaste: " + Ropa[3].nombre + ", el total de tu pedido es de $" + Ropa[3].importe * cantidad);
     } else {
         alert("No contamos con ese producto");
     };
